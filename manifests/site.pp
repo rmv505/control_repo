3e5b6c0be554e4d76b7a38fd9223ed2653bb3fb8
rmv505 <rosa.vicente@york.ac.uk>
2022-07-21 14:12:13 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,9 @@ node default {
 node 'master.puppet.vm' {
  include role::master_server
 }
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
 node /^web/ {
  include role::app_server
 }
